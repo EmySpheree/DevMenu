2b5bc213d7d59897aec4cf90b8bc3dc527b3d92b
_Emy <157907300+EmySpheree@users.noreply.github.com>
2024-01-27 19:41:54 +0100

    Add files via upload
    Es_Mapper
    Rcore_Gang
    Safezone builder
    RadarBuilder
    
    Script non fournis avec, a telecharger a coté

---
+++ config.lua
@@ -0,0 +1,31 @@
+Config = {}
+
+Config = {
+    ------------
+    ESXTrigger = "esx:getSharedObject",
+    ------------
+    ColorMenuR = 255, -- Bannière couleur R
+    ColorMenuG = 11, -- Bannière couleur G
+    ColorMenuB = 11, -- Bannière couleur B
+    ColorMenuA = 150, -- Bannière couleur A
+    MenuPositionX = 0,
+    MenuPositionY = 0,
+    MenuName = "Dev Menu", 
+    SubName = "Intéractions",
+    ------------
+    AdminRanks = {"admin","superadmin","owner","_dev","god"}, --- Change for your server
+    ------------
+    DiscordName = "DevMenu",
+    DiscordLogo = "https://resize-europe1.lanmedia.fr/r/622,311,forcex,center-middle/img/var/europe1/storage/images/europe1/international/le-panda-geant-nest-plus-en-danger-mais-reste-menace-2837755/28733065-1-fre-FR/Le-panda-geant-n-est-plus-en-danger-mais-reste-menace.jpg", -- He must finish by .png or .jpg,
+    DiscordWebHook = "",
+    ------------
+    ---- perm menu categories
+    ---- User your admin ranks line 16
+    Gestionsoi = {"admin","superadmin","owner","_dev","god"},
+    ------------
+    ColorCoords_R = 225,
+    ColorCoords_G = 55,
+    ColorCoords_B = 55,
+    ColorCoords_A = 255,
+}
+
